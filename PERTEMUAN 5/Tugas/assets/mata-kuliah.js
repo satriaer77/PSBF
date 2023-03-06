87ed800a3d26d026ff086e23096788aa59d08742
satriaer77 <ppicbimasatria@gmail.com>
2023-03-06 23:48:36 +0700

    Menambahkan PERTEMUAN 3,4,5

---
+++ PERTEMUAN 5/Tugas/assets/mata-kuliah.js
@@ -0,0 +1,76 @@
+const mataKuliah= {
+    "dosen" : {
+        "aine" :
+        {
+            "nama" : "Aine Ingram,ST., M.Eng",
+            "deskripsi" : `Aine Ingram adalah seorang dosen wanita yang lahir di Jakarta pada tanggal 18 Oktober 1980. 
+            Ia meraih gelar Sarjana Teknik (ST) dari Universitas Indonesia pada tahun 2002. Setelah itu, ia melanjutkan pendidikan dan meraih gelar Magister Teknik (M.Eng) dari Universitas Gadjah Mada pada tahun 2005.`,
+            "foto" : "aine.png"
+        },
+        "sabrina" :
+        {
+            "nama" : "Sabrina Poole,S.Kom., M.Kom",
+            "deskripsi" : `Sabrina Poole, S.kom, M.kom adalah seorang dosen di Departemen Teknik Informatika, Fakultas Ilmu Komputer Universitas Jaca Abadi. 
+            Dia lahir pada tanggal 15 Mei 1985 di Surabaya dan tumbuh besar di keluarga yang berpengaruh di bidang pendidikan.`,
+            "foto" : "sabrina.png"
+        },
+        "celine" :
+        {
+            "nama" : "Celine Becker,ST., MT",
+            "deskripsi" : `Celine Becker adalah seorang dosen wanita yang telah mengajar di bidang teknik di Universitas Jaca Abadi selama lebih dari 10 tahun. 
+            Celine Becker lahir di Jakarta pada tahun 1980 dan menyelesaikan pendidikannya di Universitas Indonesia, di mana dia memperoleh gelar Sarjana Teknik (ST) pada tahun 2002 dan gelar Magister Teknik (MT) pada tahun 2004.`,
+            "foto" : "celine.png"
+        },
+        "warren" :
+        {
+            "nama" : "Warren Owen,S.Kom., M.Kom",
+            "deskripsi" : `Warren Owen adalah seorang dosen wanita yang memiliki gelar S.kom dan M.kom dari Universitas Teknologi Surabaya (UTS). Lahir pada tanggal 1 Januari 1985.
+            Setelah menyelesaikan gelar S.kom-nya, Amanda memutuskan untuk melanjutkan pendidikannya dan memperoleh gelar M.kom dari UTS. Selama studinya di program pascasarjana, Amanda fokus pada pengembangan perangkat lunak dan aplikasi mobile.`,
+            "foto" : "warren.png"
+        },
+    },
+    "daftar" : {
+            
+            "penambangan-data" : {
+                "nama-matkul" : "Penambangan Data",
+                "cover" : "penambangan.png",
+                "dosen" : "aine",
+                "deskripsi" : `Salah satu masalah pada penambangan data adalah bagaimana cara menangani data tidak terstruktur, seperti teks dan gambar, dengan algoritma-algoritma Machine Learning. Terdapat masalah-masalah spesifik dalam pengubahan data tidak terstruktur ini menjadi data terstruktur. Contohnya adalah “Bagaimana caranya agar proses machine learning kita tidak tertipu oleh sinonim” pada teks, atau “Bagaimana agar proses machine learning kita mampu mengenali objek dalam gambar yang telah dirotasi atau tertutup sebagian”.
+                Dengan demikian, MK Penambangan Data juga akan memberi pengantar mengenai cara-cara dasar penanganan masalah-masalah tersebut serta menjelaskan dengan lebih detil beberapa algoritma Machine Learning lanjut (yang belum dipelajari pada mata kuliah Pengantar Penambangan Data). Contoh algoritma yang akan dibahas adalah keluarga Agglomerative Clustering yaitu DBSCAN dan OPTICS. Pada mata kuliah ini juga akan dibahas  pengantar terhadap Jaringan Syaraf Tiruan, yang merupakan dasar dari Deep Learning.
+                Peserta MK kuliah ini perlu sudah pernah menempuh kuliah Pengantar Penambangan Data serta memiliki pengetahuan yang mencukupi mengenai algoritma dan struktur data. Bahasa pemrograman yang digunakan dalam mata kuliah ini tidak dibatasi namun disarankan untuk sudah memahami atau mampu belajar sendiri bahasa Python.`
+            },
+            
+            "rekayasa-perangkat-lunak" : {
+                "nama-matkul" : "Rekayasa Perangkat Lunak",
+                "cover" : "rekayasa.png",
+                "dosen" : "sabrina",
+                "deskripsi" : `Mata kuliah ini merupakan mata kuliah dasara dan menjadi mata kuliah pra syarakt untuk mengambil mata kuliah selanjutnya seperti mata kuliah Analisis dan Perancangan Sistem Multimedia, Project 1, Project 2, dan Manajemen Project Multimedia. 
+                Materi pada mata kuliah ini sangat dibutuhkan oleh mahasiswa dalam mengerjakan Proyek Akhir nantinya pada semester akhir, karena materi yang diberikan meliputi tahap-tahap pengembangan sistem dengan memilih metode dan peralatan yang tepat. `
+            },
+            "kecerdasan-komputasional" : {
+                "nama-matkul" : "Kecerdasan Komputasional",
+                "cover" : "komputasional.png",
+                "dosen" : "celine",
+                "deskripsi" : `Kecerdasan Komputasional (Computational Intellegent) adalah
+                pendekatan yang muncul untuk komputasi secara paralel yang
+                meniru kemampuan manusia seperti berpikir dan belajar dalam suatu
+                lingkungan dengan ketidakpastian. Kecerdasan Komputasional adalah
+                sebuah pendekatan baru untuk membangun sistem komputasi sistem
+                cerdas yang bertujuan membantu menyelesaikan permasalahan
+                manusia. Sistem cerdas ini diharapkan memiliki kemampuan seperti
+                manusia dalam spesifikasi tertentu, dapat beradaptasi secara mandiri
+                dan belajar agar lebih baik dalam kondisi lingkungan yang berubah,
+                serta dapat menjelaskan bagaimana keputusan itu diambil.`
+            },
+            "grafika-komputer" : {
+                "nama-matkul" : "Grafika Komputer",
+                "cover" : "grafika.png",
+                "dosen" : "warren",
+                "deskripsi" : `Grafika komputer (bahasa Inggris: computer graphics) adalah bagian dari ilmu komputer yang berkaitan dengan pembuatan dan manipulasi gambar (visual) secara digital. 
+                Bentuk sederhana dari grafika komputer adalah grafika komputer 2D yang kemudian berkembang menjadi grafika komputer 3D, pemrosesan citra (image processing), dan pengenalan pola (pattern recognition). 
+                Grafika komputer sering dikenal juga dengan istilah visualisasi data.`
+            }
+        }
+};
+
+console.log
